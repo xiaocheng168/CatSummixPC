61a5d709ea46358056c19e3584c049faa91bfd8b
Zcc <x3513312668@gmail.com>
2023-07-19 02:24:19 +0800

    优化了 代码结构 将部分方法封装在对象里

--- App.js
+++ App.js
@@ -63,7 +63,7 @@ socket.on('message', (buffer, info) => {
         //匹配完成，手机端已传输的密钥
         case 4: {
             configJson.drivers[jsonData.derive] = jsonData.d
-            notification(`设备连接 (${jsonData.d.id})`, `手机端已同意，匹配成功!`)
+            app.notify(`设备连接 (${jsonData.d.id})`, `手机端已同意，匹配成功!`)
             saveConfig()
             break
         }
@@ -80,13 +80,27 @@ const network = {
             t: type, time: Date.now(), d: data, data: data, derive: `${os.hostname()}(${os.type()})`
         }
         socket.send(Buffer.from(JSON.stringify(d), 'utf-8').toString('base64'), port, address)
+    },
+    startService() {
+        //尝试绑定！3333 端口
+        socket.bind(listenerPort, "0.0.0.0", () => {
+            console.log(`listener in ${socket.address().address}:${socket.address().port} ! start successfully!`)
+            app.notify(undefined, `跨平台协同启动成功!`)
+        })
+
+        //当监听端口错误事触发的Event
+        socket.on('error', (err) => {
+            app.notify(`启动错误!`, `${err.message}`)
+            //禁止重复启动应用
+            process.exit(0)
+        })
     }
 }
 
 app.on('ready', () => {
-
     //给app加通知方法
-    app.notify = function notify(title = "兮兮互联", message, callback) {
+    app.notify = function notify(title = "兮兮互联", message, callback = () => {
+    }) {
         const notification = new Notification({
             title: title, body: message, icon: path.join(__dirname, "application.ico")
         });
@@ -94,65 +108,53 @@ app.on('ready', () => {
         notification.show()
     }
 
-
     //设置应用包名
     app.setAppUserModelId('cc.mcyx.catsummix')
-    //尝试绑定！3333 端口
-    socket.bind(listenerPort, "0.0.0.0", () => {
-        console.log(`listener in ${socket.address().address}:${socket.address().port} ! start successfully!`)
-        app.notify(undefined, `跨平台协同启动成功!`)
 
-        //设置托盘图标
-        const tray = new Tray(path.join(__dirname, "application.ico"));
+    //设置托盘图标
+    const tray = new Tray(path.join(__dirname, "application.ico"));
 
-        //设置menu
-        function setTrayMenu() {
-            tray.setContextMenu(Menu.buildFromTemplate([{
-                label: '关于', type: 'normal', click: () => {
-                    dialog.showMessageBoxSync({
-                        title: "关于项目",
-                        message: "这是一个 Node.js 平台 Electron框架搭建的项目",
-                        type: 'question',
-                        icon: path.join(__dirname, "application.ico")
-                    })
-                }
-            }, {
-                label: `设置自启动服务`, type: 'normal', click: () => {
-                    const loginItemSettings = app.getLoginItemSettings();
-                    //设置开机自启
-                    app.setLoginItemSettings({
-                        openAtLogin: !loginItemSettings.openAtLogin,
-                        openAsHidden: false,
-                        path: process.execPath,
-                        args: []
-                    })
-                    app.notify(undefined, loginItemSettings.openAtLogin ? '已安装开机自启服务' : '已卸载开启自启服务')
-                }
-            }, {
-                label: '退出', type: 'normal', click: () => {
-                    app.notify(undefined, "程序已关闭!")
-                    //销毁托盘
-                    tray.destroy()
-                    //结束进程
-                    process.exit(0)
-                }
-            }]))
-        }
+    //设置menu
+    function setTrayMenu() {
+        tray.setContextMenu(Menu.buildFromTemplate([{
+            label: '关于', type: 'normal', click: () => {
+                dialog.showMessageBoxSync({
+                    title: "关于项目",
+                    message: "这是一个 Node.js 平台 Electron框架搭建的项目",
+                    type: 'question',
+                    icon: path.join(__dirname, "application.ico")
+                })
+            }
+        }, {
+            label: `设置自启动服务`, type: 'normal', click: () => {
+                const loginItemSettings = app.getLoginItemSettings();
+                //设置开机自启
+                app.setLoginItemSettings({
+                    openAtLogin: !loginItemSettings.openAtLogin,
+                    openAsHidden: false,
+                    path: process.execPath,
+                    args: []
+                })
+                app.notify(undefined, loginItemSettings.openAtLogin ? '已安装开机自启服务' : '已卸载开启自启服务')
+            }
+        }, {
+            label: '退出', type: 'normal', click: () => {
+                app.notify(undefined, "程序已关闭!")
+                //销毁托盘
+                tray.destroy()
+                //结束进程
+                process.exit(0)
+            }
+        }]))
+    }
 
-        //设置托盘图标
-        setTrayMenu()
-        //设置托盘标题
-        tray.setToolTip("CatSummix")
-    })
+    //设置托盘图标
+    setTrayMenu()
+    //设置托盘标题
+    tray.setToolTip("CatSummix")
 
-    //当监听端口错误事触发的Event
-    socket.on('error', (err) => {
-        new Notification({
-            title: `启动错误!`, body: `${err.message}`, icon: path.join(__dirname, "application.ico")
-        }).show()
-        //禁止重复启动应用
-        process.exit(0)
-    })
+    //启动服务
+    network.startService()
 })
 
 
